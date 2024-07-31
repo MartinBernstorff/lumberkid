71227a7c73469536b6282e861fdf6f0991cd15ae
Martin Bernstorff <martinbernstorff@gmail.com>
2024-07-31 21:19:59 +0200

    update commands.py

--- src/lumberkid/commands.py
+++ src/lumberkid/commands.py
@@ -11,13 +11,13 @@ def add() -> None:
     cfg = get_config()
 
     source = cfg.issue_source.setup()
-    issues = [
-        *source.get_latest(cfg.in_progress_label),
-        *source.assigned_to_me(cfg.in_progress_label),
-    ]
-    selected = IssueSelecter(cfg.issue_title_parser).select_issue_dialog(issues)
+    selected = IssueSelecter(cfg.issue_title_parser).select_issue_dialog(
+        [*source.get_latest(cfg.in_progress_label), *source.assigned_to_me(cfg.in_progress_label)]
+    )
+
     cfg.vcs.add(selected)
     cfg.forge.add(selected)
+
     if cfg.sync_on == SyncOn.ALL:
         sync()
 
